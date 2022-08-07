8a5883ae6e2774ad23b87a4a731a641cbaee324b
Kotaro <71119362+routine-works@users.noreply.github.com>
2022-08-08 08:29:19 +0900

    set background img

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./notFound.css"
 
+const numImagesAvailable = 10;
+const imageWidth = "500px";
+const imageHeight = "500px";
+const collectionID = "kj18-xEoTvo";
+  
 function NotFound () {
+
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(()=>{
+    const randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+    console.log(collectionID);
+    fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomImageIndex}`).then(data => {
+      setBackgroundImage(data.url);
+      console.log(data.url);
+    })
+  }, []);
+
   return (
-    <p>404 not found</p>
+    <main id="404" style={{ 
+      backgroundImage: `url(${backgroundImage})` ,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "top center",
+      backgroundSize: "cover"
+    }}>
+      <p>404 Not Found<br />お探しのページは見つかりませんでした</p>
+    </main>
   )
 }
 
